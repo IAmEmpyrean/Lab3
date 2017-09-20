5805d586f573faa221a79a846bfdcb12c3e70b7b
Andrew Obrzut <byk121@gmail.com>
2017-09-20 11:56:28 -0500

    utilized urlToString

--- src/main/java/Mroz.java
+++ src/main/java/Mroz.java
@@ -13,7 +13,14 @@ public class Mroz {
 
 
     public static void main(String[] args) {
-        System.out.println("Mroz");
+        System.out.println("Mroz had a little lamb");
+        String url = "http://erdani.com/tdpl/hamlet.txt";
+        String urlString = urlToString(url);
+        System.out.println(urlString);
+
+
+
+
     }
 
 
@@ -24,7 +31,7 @@ public class Mroz {
      * @param url url to retrieve contents from
      * @return the contents from the url as a string, or an empty string on error
      */
-    public static String urlToString(final String url) {
+    public static final String urlToString(final String url) {
         Scanner urlScanner;
         try {
             urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
